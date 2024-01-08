b578043873bfbab8ace56c881580ad5a8ef03ee7
albert1413 <145456779+albert1413@users.noreply.github.com>
2024-01-08 10:50:58 +0100

    1.3

--- temperatura.py
+++ temperatura.py
@@ -1,18 +1,20 @@
 def main():
+    '''Identificar variables'''
     a = int(input("Escriu la temperatura en Graus Celsius:"))
 
+    '''Calent'''
     if a>30:
         if a>=100:
             print("Estàs hot. L'aigua està bullint.")
         else:
             print("Estàs hot")
-
+    '''Fred'''
     if a<10:
         if a<=0:
             print("Pecho frio. L'aigua és congelarà.")
         else:
             print("Pecho frio")
-
+    '''Normal'''
     if 10<=a<=30:
         print("Estàs god")
 
