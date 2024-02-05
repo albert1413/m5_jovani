086fff8b7760b730d64d49074e74ce94a7bf6156
albert1413 <albertjovani@iesmontsia.org>
2024-02-05 09:32:58 +0100

    1.3

--- CostPersonal.py
+++ CostPersonal.py
@@ -1,11 +1,12 @@
 from classTreballador import Treballador
 
 def calculaCostDelPersonal(treballadors: list):
+    PreuHoresExtra = 20
     costFinal = 0
     for treballador in treballadors:
         if treballador.getTipusTreballador() in [Treballador.DIRECTOR,Treballador.SUBDIRECTOR]:
             costFinal += treballador.getNomina()
         else:
-            costFinal += treballador.getNomina() + (treballador.getHoresExtres()*20)
+            costFinal += treballador.getNomina() + (treballador.getHoresExtres()*PreuHoresExtra)
 
     return costFinal
